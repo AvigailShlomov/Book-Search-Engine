a5addd6a6a56c8c3408a7f543460070cf9435aa5
AvigailShlomov <anm798@gmail.com>
2025-11-01 20:06:44 +0200

    Feat: added common service

---
+++ src/app/core/services/common.service.ts
@@ -0,0 +1,13 @@
+import { inject, Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CommonService {
+  private _snackBar = inject(MatSnackBar);
+
+  openSnackBar(message: string, action: string, config: number) {
+    this._snackBar.open(message, action, { duration: config });
+  }
+}
